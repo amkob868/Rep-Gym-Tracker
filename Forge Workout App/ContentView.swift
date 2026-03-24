cf664f02e056d6555721c821923c7226fb4bdc55
amkob868 <122923830+amkob868@users.noreply.github.com>
2026-03-24 16:33:15 -0700

    Initial Commit

---
+++ Forge Workout App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Forge Workout App
+//
+//  Created by Alexander Kobinski on 3/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Forge Workout App/Forge_Workout_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Forge_Workout_AppApp.swift
+//  Forge Workout App
+//
+//  Created by Alexander Kobinski on 3/24/26.
+//
+
+import SwiftUI
+
+@main
+struct Forge_Workout_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
